e41f9748f7036f8692883875d63b787891e97794
Garlen <649975243@qq.com>
2026-06-03 14:14:54 +0800

    chore: base配置调整

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,8 +3,6 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/blog/",
-
   lang: "zh-CN",
   title: "Garlen",
   description: "vuepress-theme-hope 的博客演示",
